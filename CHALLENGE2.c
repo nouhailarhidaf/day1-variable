28eb6a399bc38135b3e9d36b6f8ede5139b16c14
nouhaila-rhidaf <nouhailarhidaf04@gmail.com>
2024-09-24 16:05:23 +0100

    CHALLENGE2

---
+++ CHALLENGE2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+
+float c,k;
+
+    printf("veuillez entrer la température en c:");
+    scanf("%f",&c);
+    k=c + 273.15;
+    printf(" la temperature en Kelvin %f ",k);
+    return 0;
+
+}
